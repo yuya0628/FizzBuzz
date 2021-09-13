e1e70a500fa8661751e53987ebbea85da1885ff8
yuya0628 <sonoyuki13@gmail.com>
2021-09-13 20:16:19 +0900

    fizzbuzz

---
+++ FizzBuzz.rb
@@ -0,0 +1,26 @@
+
+
+x = gets.to_i
+
+for t in (1..x)
+
+  if t%15 == 0
+    puts "fizzbuzz"
+  elsif t%5 == 0
+    puts "buzz"
+  elsif t%3 == 0
+    puts "fizz"
+  else 
+    puts t 
+  end  
+
+end  
+
+
+
+#まとめ(小⇨大、定数⇨変数で考えるとうまくいく)
+
+#１st．まず、ある１つの数字xで考える
+#２nd．x=定数で作る
+#３rd．xを変数にする
+#４th．1~xまでの複数の数字で考える
